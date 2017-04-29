6eb7ddadb7105c0162860027a9a6f4a393549c21
Shaojung <shaojung@gmail.com>
2017-04-29 09:41:12 +0800

    add pending Intent

--- app/src/main/java/com/example/teacher/db042901/MainActivity.java
+++ app/src/main/java/com/example/teacher/db042901/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.teacher.db042901;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,9 +23,14 @@ public class MainActivity extends AppCompatActivity {
     public void click1(View v)
     {
         Notification.Builder builder = new Notification.Builder(context);
+        Intent it = new Intent(MainActivity.this, DetailActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(context, 2, it, PendingIntent.FLAG_UPDATE_CURRENT);
+
         builder.setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("Title")
-                .setContentText("內文");
+                .setContentText("內文")
+                .setContentIntent(pi)
+                .setAutoCancel(true);
         manager.notify(1, builder.build());
 
 
